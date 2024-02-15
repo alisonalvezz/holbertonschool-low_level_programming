05cb853c9f6f5cacbddf1deb3d445642aab91258
Alison <8661@holbertonstudents.com>
2024-02-15 16:56:44 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printf
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	Return(0);
+}
