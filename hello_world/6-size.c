2977f17ca6c4027cbbed2fe4456e2b1d54fc7ff6
Alison <8661@holbertonstudents.com>
2024-02-16 12:26:24 +0000

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printf sizeof char, int, float, double
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return (0);
+}
+
