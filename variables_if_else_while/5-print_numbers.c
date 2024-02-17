52c5f19bce961f9181653458c4c3949ca69c16b2
Alison <8661@holbertonstudents.com>
2024-02-17 19:34:56 +0000

    print

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - int 1 to 10
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int number = '0';
+
+		for (number = '0'; number <= '9'; number++)
+
+			printf("%d", number);
+
+		printf("/n");
+
+		return (0);
+}
+
