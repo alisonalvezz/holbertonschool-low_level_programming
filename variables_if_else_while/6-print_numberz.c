8dad331d08d8dd27a85cefa15c4d4fc10d6c4719
Alison <8661@holbertonstudents.com>
2024-02-17 21:58:48 +0000

    print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print number 0 to 9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number < 10; number++)
+{
+		putchar(number);
+}
+	putchar('\n');
+
+	return (0);
+}
+
