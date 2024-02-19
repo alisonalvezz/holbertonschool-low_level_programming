eb8713f198ac89120baa87016cdc992705973962
Alison <8661@holbertonstudents.com>
2024-02-19 19:07:57 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * _abs - The absolute valor
+ *
+ * @n: the integer
+ *
+ * Return: the absolute value of the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+		return (-n);
+}
