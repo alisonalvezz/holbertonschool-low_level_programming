db4f5579ea62511d7b05fa751da58de9e766f38c
Alison <8661@holbertonstudents.com>
2024-02-19 17:11:29 +0000

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_sign - greater than zero 1, zero 0, less tan zero -1
+ *
+ * Return: greater than zero 1, zero 0, less than zero -1
+ *
+ * @n: number, zero, greater than zero or less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if(n == 0)
+		return (0);
+	else
+		return (-1);
+
+}
