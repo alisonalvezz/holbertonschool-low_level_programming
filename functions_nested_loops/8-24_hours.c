971ee76adb7b23be83ed334d2e9bd42ca65b947a
Alison <8661@holbertonstudents.com>
2024-02-20 11:53:25 +0000

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * jack_bauer - prints all hours and minutes in the day
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+	}
+}
