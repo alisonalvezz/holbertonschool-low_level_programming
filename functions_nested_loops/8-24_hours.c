b6eb758075c5527290ad0602a1fe19abad1225fa
Alison <8661@holbertonstudents.com>
2024-02-20 11:58:41 +0000

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,10 +16,14 @@ void jack_bauer(void)
 	for (h = 0; h <= 23; h++)
 	{
 		for (m = 0; m <= 59; m++)
+		{
 		_putchar((h / 10) + '0');
 		_putchar((h % 10) + '0');
+		_putchar(':');
 		_putchar((m / 10) + '0');
 		_putchar((m % 10) + '0');
 		_putchar('\n');
+		}
+		
 	}
 }
