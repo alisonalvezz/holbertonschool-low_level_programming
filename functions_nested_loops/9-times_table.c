0ee737927464a5fd824511be5bb60fd2fa14ddf7
Alison <8661@holbertonstudents.com>
2024-02-20 13:14:10 +0000

    24 hours

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ *
+ * times_table - print the 9 times table
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int n, m, p;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n);
+
+		for (m = 0; m <= 9; m++)
+		{
+			_putchar(m);
+			p = n * m;
+			_putchar(p);
+			_putchar(',');
+			_putchar('.');
+			_putchar('.');
+		}
+
+		if (p <= 9)
+			_putchar(',');
+
+
+	}
+
+		_putchar('\n');
+}
