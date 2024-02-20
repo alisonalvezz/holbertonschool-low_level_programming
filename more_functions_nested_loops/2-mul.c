7a08246211e0e6a56ac9bdebcddf30dfdfc75f34
Alison <8661@holbertonstudents.com>
2024-02-20 18:31:08 +0000

    numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * mul - multiplies a and b
+ *
+ *@a: variable
+ *@b: variable
+ * Return: result of multiply a and b
+ */
+
+int mul(int a, int b)
+{
+	return (int a + int b);
+
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ *@n: from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int n
+		for (n >= 0 && n <= 9 && n++)
+			_putchar('n');
+
+	_putchar('\n');
+}
