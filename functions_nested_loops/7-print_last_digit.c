351bfa2eff2364d32e783ea884f82424d8c95906
Alison <8661@holbertonstudents.com>
2024-02-20 01:10:42 +0000

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @n: digit
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	if ((n % 10) <= 0)
+	{
+		return (n);
+	} return (-n);
+}
