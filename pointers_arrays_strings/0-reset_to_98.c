c86f0a8049bbe1bd2e40650127df6e1a2bc26e95
Alison <8661@holbertonstudents.com>
2024-02-22 13:23:50 +0000

    swap

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
  /**
-  * reset_to_98: Updates the value of n to 98
+  *  reset_to_98 - Updates the value of n to 98.
+  *
+  *  @n: the pointer to an int.
   */
 
 void reset_to_98(int *n)
