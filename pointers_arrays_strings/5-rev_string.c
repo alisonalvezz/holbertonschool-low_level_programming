f21967acd09ceacd43a94aaf6794229ac9dcd3cc
Alison <8661@holbertonstudents.com>
2024-02-24 17:27:00 +0000

    rev string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *@s: the string
+ */
+
+void rev_string(char *s)
+{
+	int l, a;
+	char b;
+
+	l= 0;
+
+	while (s[l] != '\0')
+		l++;
+	l--;
+
+	for (a = 0; a <= l / 2; a++)
+	{
+		b = s[a];
+		s[a] = s[l - a];
+		s [l - a] = b;
+	}
+}
