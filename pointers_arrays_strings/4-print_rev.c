c15c9e3445dbda6d89d65347fbf02576a19265e0
Alison <8661@holbertonstudents.com>
2024-02-24 14:34:03 +0000

    betty style added

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,15 @@
 /**
  * print_rev - prints in reverse a string
  *
- * @s: the pointer
+ *@s: the pointer
  */
 
 void print_rev(char *s)
 {
 	int rev, b;
 
-	for (rev = 0; s[rev]; rev++);
+	for (rev = 0; s[rev]; rev++)
+		;
 	b = rev;
 
 	for (b = rev - 1; b >= 0; b--)
