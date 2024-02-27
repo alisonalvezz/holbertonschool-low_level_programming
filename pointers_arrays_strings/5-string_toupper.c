5dcdf8f7354cf3b89e76bdcd294bf979a4364296
Alison <8661@holbertonstudents.com>
2024-02-27 12:58:44 +0000

    string toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,8 +11,8 @@ char *string_toupper(char *s)
 	int a;
 
 	for (a = 0; s[a] != '\0'; a++)
-		if (s[a] >= 'a' && s[a])
-				(s[a] = a[s] - 3);
+		if (s[a] >= 'a' && s[a] <= 'z')
+				s[a] = a[s] - 32;
 
 	return (s);
 }
