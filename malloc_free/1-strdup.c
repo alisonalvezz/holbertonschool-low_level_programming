66390cda98303aa9038e8e950c361fd9c38e41bf
Alison <8661@holbertonstudents.com>
2024-03-01 17:06:52 +0000

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str)
+{
+	const char *string;
+
+	char *dest;
+
+	char *strcpy(char *dest, const char *string);
+
+	*str = dest;
+
+	for (dest = 0; dest > string; dest++)
+		*str = dest;
+
+	for (dest = 0; dest < string)
+		return (NULL);
+}
