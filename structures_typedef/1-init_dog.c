b0b83b5c2ca461b13648a9407350573c84d3b2a4
Alison <alisonalvez05@gmail.com>
2024-03-14 00:14:23 +0000

    dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -6,14 +6,15 @@
  * @name: name of the dog
  * @owner: owner of the dog
  * @age: age of the dog
+ * @d: d
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d-> name = name;
-		d-> age = age;
-		d-> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
