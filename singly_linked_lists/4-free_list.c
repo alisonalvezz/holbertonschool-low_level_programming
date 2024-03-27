eeb4271630d0879e00393fc7260193f05283dd02
Alison <8661@holbertonstudents.com>
2024-03-27 03:51:46 +0000

    commit

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
+/**
+ * free_list - frees a list
+ * @head: head
+ */
+
 void free_list(list_t *head)
 {
 	list_t *adr;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		adr = head->next;
 		free(head->str);
