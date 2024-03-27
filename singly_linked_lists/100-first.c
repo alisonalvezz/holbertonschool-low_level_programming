af2319f6eeb582f085e325a83a772777ea31e267
Alison <8661@holbertonstudents.com>
2024-03-27 04:08:19 +0000

    commit

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) before_main() {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+/**
+ * before_main - function that print before main is executed
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
