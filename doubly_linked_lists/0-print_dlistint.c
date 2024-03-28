9c7bddd9fef0f1b6df2c9b847aa344944336e82a
Alison <8661@holbertonstudents.com>
2024-03-28 19:51:08 +0000

    commit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a list
+ * @h: h
+ * Return: return
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
